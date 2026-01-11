95c0ed7bb86efe1407594aee2854c6bdf405b6a6
mirror-2922 <208280089+mirror-2922@users.noreply.github.com>
2026-01-11 23:45:53 +0800

    :wrench: chore: suppress excessive CameraX logs and configure Application class

---
+++ app/src/main/java/com/mirror2922/ecvl/BeautyApplication.kt
@@ -0,0 +1,14 @@
+package com.mirror2922.ecvl
+
+import android.app.Application
+import android.util.Log
+import androidx.camera.camera2.Camera2Config
+import androidx.camera.core.CameraXConfig
+
+class BeautyApplication : Application(), CameraXConfig.Provider {
+    override fun getCameraXConfig(): CameraXConfig {
+        return CameraXConfig.Builder.fromConfig(Camera2Config.defaultConfig())
+            .setMinimumLoggingLevel(Log.ERROR)
+            .build()
+    }
+}
